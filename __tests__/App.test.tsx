439a69c1ff32c936435ac5e5b6a5aac410061786
shreya-mishra <shreyamishra040398@gmail.com>
2024-02-01 11:03:31 +0530

    added mapping tank frame testcase and testids for tank frame and other views as well

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import App from '../src/App';
+import {arr} from '../src/constant/TankConstants';
+import {COLORS} from '../src/constant/Colors';
+import '@testing-library/jest-native/extend-expect';
 
 test('renders correctly', () => {
-  const {getByText} = render(<App />);
+  const {getByText, getByTestId} = render(<App />);
   const linkElement = getByText(/Water Tank Simulation/i);
   expect(linkElement).toBeTruthy();
+  // check if each tank is rendered
+  for (let i = 0; i < arr.length - 1; i++) {
+    const tankFrame = getByTestId(`tank-frame ${i}`);
+
+    expect(tankFrame).toBeDefined();
+  }
 });
